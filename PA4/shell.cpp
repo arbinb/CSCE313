b301d995af330587448e6b85238a9df0bc8be9df
Arbin Bhuiyan <arbin.bhuiyan@gmail.com>
2019-11-10 03:01:17 +0000

    pipes and redirection ACTUALLY working, cd implemented as well

--- PA4/shell.cpp
+++ PA4/shell.cpp
@@ -1,4 +1,5 @@
 #include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
@@ -33,13 +34,14 @@ string trim(string input) {  //trims anything before and including leading space
 }
 
 vector<string> split(string line, string separator = " ", vector<string> in = vector<string>(0, "")) {  //splits a string delimited by a separator into a vector
-    //cout << in.size() << endl;
+    ////cout << in.size() << endl;
     vector<string> result = in;
     while (line.size()) {
+        int length = line.length();
         size_t found = line.find(separator);
         if (found == string::npos) {  //If separator not found
             string lastpart = trim(line);
-            //cout << "Last part " << lastpart << endl;
+            ////cout << "Last part " << lastpart << endl;
             if (lastpart.size() > 0) {
                 result.push_back(lastpart);
             }
@@ -47,37 +49,58 @@ vector<string> split(string line, string separator = " ", vector<string> in = ve
         }
         size_t quotes_start = line.find("\"");  //ignore anything in quotes
         if (quotes_start != string::npos) {
-            //cout << line.substr(quotes_start + 1) << endl;
+            ////cout << line.substr(quotes_start + 1) << endl;
             size_t quotes_end = line.substr(quotes_start + 1).find("\"") + quotes_start + 1;
             if (quotes_end != string::npos)
-                //cout << "quotes in this bitch" << endl;
+                ////cout << "quotes in this bitch" << endl;
                 if (found < quotes_start) {  //continue as normal
-                    //cout << "< before quote" << endl;
+                    ////cout << "< before quote" << endl;
                 } else if (found > quotes_end) {  //continue as normal
-                    //cout << "< after quotes" << endl;
+                    ////cout << "< after quotes" << endl;
                 } else if ((found > quotes_start) && (found < quotes_end)) {  //if separator found within quotes ignore and look for seperator after the quotes
-                    //cout << "quotes in middle" << endl;
+                    ////cout << "quotes in middle" << endl;
                     found = line.substr(quotes_end + 1).find(separator) + quotes_end + 1;
                 }
         }
-        string segment = trim(line.substr(0, found));
-        ////cout << "line: " << line << "found: " << found << endl;
-        line = line.substr(found + 1);
-        if (separator == ">") {
-            string s2 = "@REDIROUTTO";
-            line.insert(0, s2);
-        } else if (separator == "<") {
-            string s2 = "@REDIRINFROM";
-            line.insert(0, s2);
+        quotes_start = line.find("'");  //ignore anything in quotes
+        if (quotes_start != string::npos) {
+            ////cout << line.substr(quotes_start + 1) << endl;
+            size_t quotes_end = line.substr(quotes_start + 1).find("'") + quotes_start + 1;
+            if (quotes_end != string::npos)
+                ////cout << "quotes in this bitch" << endl;
+                if (found < quotes_start) {  //continue as normal
+                    ////cout << "< before quote" << endl;
+                } else if (found > quotes_end) {  //continue as normal
+                    ////cout << "< after quotes" << endl;
+                } else if ((found > quotes_start) && (found < quotes_end)) {  //if separator found within quotes ignore and look for seperator after the quotes
+                    ////cout << "quotes in middle" << endl;
+                    found = line.substr(quotes_end + 1).find(separator) + quotes_end + 1;
+                }
         }
-        //cout << "Segment: " << segment << endl;
-        //cout << "line: " << line << endl;
+        if (found >= line.length() - 1) {
+            found += 1;
+            separator = "!@#$%^";
+        }
+        string segment = trim(line.substr(0, found));
+        //////cout << "line: " << line << "found: " << found << endl;
+        if (found != length) {
+            line = line.substr(found + 1);
+        } else {
+            line = "";
+        }
+        if (separator != " ") {
+            if (separator == ">") {
+                string s2 = "@REDIROUTTO";
+                line.insert(0, s2);
+            } else if (separator == "<") {
+                string s2 = "@REDIRINFROM";
+                line.insert(0, s2);
+            }
+        }
 
-        ////cout << "[" << segment << "]"<< endl;
-        if (segment.size() != 0)
+        if (segment.size() != 0) {
             result.push_back(segment);
-
-        ////cout << line << endl;
+        }
     }
     return result;
 }
@@ -94,15 +117,20 @@ char** vec_to_char_array(vector<string> parts) {
 }
 
 void execute(string command) {
-    vector<string> argstrings = split(command, " ");  // split the command into space-separated parts
-    char** args = vec_to_char_array(argstrings);      // convert vec<string> into an array of char*
+    vector<string> argstrings = split(command, " ");
+    //cout << "IN EXEC" << endl;  // split the command into space-separated parts
+    for (int i = 0; i < argstrings.size(); i++) {
+        argstrings.at(i).erase(std::remove(argstrings.at(i).begin(), argstrings.at(i).end(), '"'), argstrings.at(i).end());
+        argstrings.at(i).erase(std::remove(argstrings.at(i).begin(), argstrings.at(i).end(), '\''), argstrings.at(i).end());
+    }
+    char** args = vec_to_char_array(argstrings);  // convert vec<string> into an array of char*
     execvp(args[0], args);
 }
 
 vector<string> redirection(string line) {  //not actual logic but using split, creates a vector denoting redirection
-    //cout << line << endl;
+    ////cout << line << endl;
     vector<string> in_parts = split(line, "<");
-    //cout << in_parts.size() << endl;
+    ////cout << in_parts.size() << endl;
     vector<string> out_parts;
     for (string x : in_parts) {
         out_parts = split(x, ">", out_parts);
@@ -111,6 +139,13 @@ vector<string> redirection(string line) {  //not actual logic but using split, c
 }
 
 int main() {
+    char s[100];
+    char b[100];
+    cout << getcwd(s, 100) << endl;
+    chdir("..");
+    cout << getcwd(b, 100) << endl;
+    chdir(s);
+    cout << getcwd(b, 100) << endl;
     int info = 0;
     vector<string> infoCommands;
     infoCommands.push_back("echo Working Directory:");
@@ -120,26 +155,48 @@ int main() {
     infoCommands.push_back("echo Date and Time:");
     infoCommands.push_back("date");
 
+    char cwd[100];
+    char pwd[100];
+
+    getcwd(cwd, 100);
+    getcwd(pwd, 100);
+
     while (true) {                         // repeat this loop until the user presses Ctrl + C
         int std_in = dup(STDIN_FILENO);    // need to keep original stdin so it can be reset after redirection
         int std_out = dup(STDOUT_FILENO);  // need to keep original stdout so it can be reset after redirection
 
-        cout << "\nuser@shell $ ";
+        cout << "user@shell $ ";
 
         string commandline = ""; /*get from STDIN, e.g., "ls  -la |   grep Jul  | grep . | grep .cpp" */
         getline(cin, commandline);
         string checkExit = commandline;
         transform(checkExit.begin(), checkExit.end(), checkExit.begin(), ::tolower);
         if (checkExit == "exit") {
-            return 0;
+            exit(-1);
         }
-        // //cout << commandline << endl;
+
+        size_t cd = commandline.find("cd");
+        if (cd != string::npos) {
+            char cd_arg[100];
+            strcpy(cd_arg, commandline.substr(3).c_str());
+            char old[] = "-";
+            if (cd_arg[0] == old[0]) {
+                char tmp[100];
+                memcpy(tmp, pwd, sizeof(pwd));
+                memcpy(pwd, cwd, sizeof(cwd));
+                memcpy(cwd, tmp, sizeof(tmp));
+                chdir(cwd);
+            } else {
+                memcpy(pwd, cwd, sizeof(cwd));
+                chdir(cd_arg);
+                getcwd(cwd, 100);
+            }
+            char x[100];
+            cout << getcwd(x, 100) << endl;
+            continue;
+        }
         // split the command by the "|", which tells you the pipe levels
         vector<string> piped_parts = split(commandline, "|");
-        //cout << piped_parts.size() << endl;
-        for (string x : piped_parts) {
-            //cout << x << endl;
-        }
 
         // for each pipe, do the following:
         for (int i = 0; i < piped_parts.size(); i++) {
@@ -150,15 +207,41 @@ int main() {
             if (!fork()) {  //child; redirects output to parent and execute in here
                 // redirect output to the next level unless this is the last level
                 if (i < piped_parts.size() - 1) {
+                    if (redirected_parts.size() > 1) {
+                        for (int k = 0; k < redirected_parts.size(); k++) {
+                            string redirFile = redirected_parts.at(k);
+                            size_t found = redirFile.find("@REDIRINFROM");
+                            if (found != string::npos) {
+                                ////cout << "redirecting input" << endl;
+                                redirFile = redirFile.substr(13);
+                                redirFile = trim(redirFile);
+                                int inFD = open(redirFile.c_str(), O_RDONLY);
+                                dup2(inFD, STDIN_FILENO);
+                                close(inFD);
+                                piped_parts.at(i) = redirected_parts.at(0);
+                            }
+                            found = redirFile.find("@REDIROUTTO");
+                            if (found != string::npos) {
+                                ////cout << "redirecting output" << endl;
+                                redirFile = redirFile.substr(11);
+                                redirFile = trim(redirFile);
+                                int outFD = open(redirFile.c_str(), O_CREAT | O_WRONLY | O_TRUNC,
+                                                 S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+                                dup2(outFD, STDOUT_FILENO);
+                                close(outFD);
+                                piped_parts.at(i) = redirected_parts.at(0);
+                            }
+                        }
+                    }
                     dup2(fd[1], STDOUT_FILENO);            // redirect STDOUT to fd[1], so that it can write to the other side
                     close(fd[1]);                          // STDOUT already points fd[1], which can be closed
                 } else if (redirected_parts.size() > 1) {  //handles redirected output
-                    //cout << "we got some redirections" << endl;
+                    ////cout << "we got some redirections" << endl;
                     for (int j = 0; j < redirected_parts.size(); j++) {
                         string redirFile = redirected_parts.at(j);
                         size_t found = redirFile.find("@REDIROUTTO");
                         if (found != string::npos) {
-                            //cout << "redirecting output" << endl;
+                            ////cout << "redirecting output" << endl;
                             redirFile = redirFile.substr(11);
                             redirFile = trim(redirFile);
                             int outFD = open(redirFile.c_str(), O_CREAT | O_WRONLY | O_TRUNC,
@@ -169,7 +252,7 @@ int main() {
                         }
                         found = redirFile.find("@REDIRINFROM");
                         if (found != string::npos) {
-                            //cout << "redirecting input" << endl;
+                            ////cout << "redirecting input" << endl;
                             redirFile = redirFile.substr(13);
                             redirFile = trim(redirFile);
                             int inFD = open(redirFile.c_str(), O_RDONLY);
@@ -186,6 +269,7 @@ int main() {
                 /* executes function that can split the command by spaces to 
                 find out all the arguments, see the definition*/
                 if (commandline != "info") {
+                    //cout << "Got here" << endl;
                     execute(piped_parts[i]);  // this is where you execute
                 }
 
@@ -208,6 +292,6 @@ int main() {
         dup2(std_out, STDOUT_FILENO);
         close(std_out);
 
-        //cout << "Entering next iteration";
+        ////cout << "Entering next iteration";
     }
 }
